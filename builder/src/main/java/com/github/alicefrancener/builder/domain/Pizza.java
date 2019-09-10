71724be139ceedfa319dd0028409855cd6966ed3
Alice Francener <alicefrancener@gmail.com>
2019-09-09 21:13:01 -0300

    Implementa padrão builder

---
+++ builder/src/main/java/com/github/alicefrancener/builder/domain/Pizza.java
@@ -0,0 +1,119 @@
+package com.github.alicefrancener.builder.domain;
+
+
+public class Pizza {
+
+    private String tamanho;
+    private String borda;
+    private boolean queijo;
+    private boolean tomate;
+    private boolean calabresa;
+    private boolean cebola;
+
+        public static class Builder {
+            private String tamanho;
+            private String borda;
+            private boolean queijo;
+            private boolean tomate;
+            private boolean calabresa;
+            private boolean cebola;
+
+            /**
+             * O tamanho da Pizza é obrigatório.
+             *
+             * @param tamanho o tamanho é obrigatório.
+             */
+            public Builder(final String tamanho) {
+                this.tamanho = tamanho;
+            }
+
+            public Builder addBorda(final String borda) {
+                this.borda = borda;
+                return this;
+            }
+
+            public Builder addQueijo(final boolean queijo) {
+                this.queijo = queijo;
+                return this;
+            }
+
+            public Builder addTomate(final boolean tomate) {
+                this.tomate = tomate;
+                return this;
+            }
+
+            public Builder addCalabresa(final boolean calabresa) {
+                this.calabresa = calabresa;
+                return this;
+            }
+
+            public Builder addCebola(final boolean cebola) {
+                this.cebola = cebola;
+                return this;
+            }
+
+            /**
+             * Cria uma instância da classe {@link Pizza} conforme os parâmetros
+             * fornecidos.
+             *
+             * @return Uma instância da classe {@link Pizza} conforme os
+             * parâmetros fornecidos.
+             */
+            public Pizza build() {
+                return new Pizza(this);
+            }
+        }
+
+        /**
+         * Construtor PRIVADO que faz uso de objeto da classe {@link Builder}.
+         *
+         * @param builder Objeto contendo valores a partir dos quais a instância
+         *                da classe {@link Pizza} será criada.
+         */
+        private Pizza(Builder builder) {
+            tamanho = builder.tamanho;
+            borda  = builder.borda;
+            queijo  = builder.queijo;
+            tomate = builder.tomate;
+            calabresa = builder.calabresa;
+            cebola = builder.cebola;
+        }
+
+        public String getTamanho() {
+            return tamanho;
+        }
+
+        public String getBorda() {
+            return borda;
+        }
+
+        public String getQueijo() {
+            return queijo ? "com queijo" : "sem queijo";
+        }
+
+        public String getTomate() {
+            return tomate ? "com tomate" : "sem tomate";
+        }
+
+    public String getCalabresa() {
+        return calabresa ? "com calabresa" : "sem calabresa";
+    }
+
+    public String getCebola() {
+        return cebola ? "com cebola" : "sem cebola";
+    }
+
+        /**
+         * Produz a pizza completa.
+         * @return A pizza completa.
+         */
+        @Override
+        public String toString() {
+            final String parcial = String.join(" ", "Pizza:",
+                    tamanho, borda, getQueijo(), getCalabresa(), getCebola(),
+                    getTomate());
+
+            // Remove espaços caso não haja título, sobrenomes e/ou sufixos
+            return parcial.trim().replaceAll(" +", " ");
+        }
+}
---
+++ builder/src/test/java/com/github/alicefrancener/builder/domain/PizzaTest.java
@@ -0,0 +1,33 @@
+package com.github.alicefrancener.builder.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class PizzaTest {
+
+    @Test
+    public void builderCasoTrivial() {
+        Pizza.Builder pizza = new Pizza.Builder("grande")
+                .addBorda("catupiry");
+        assertEquals("Pizza: grande catupiry sem queijo sem calabresa "
+                + "sem cebola sem tomate", pizza.build().toString());
+    }
+
+    @Test
+    void verificaBuilder() {
+        Pizza.Builder builder = new Pizza.Builder("pequena");
+        Pizza pizza = builder
+                .addBorda("sem borda")
+                .addTomate(true)
+                .addQueijo(false)
+                .addCebola(true)
+                .addCalabresa(true)
+                .build();
+
+        assertEquals("Pizza: pequena sem borda sem queijo com "
+                + "calabresa com cebola com tomate", pizza.toString());
+    }
+
+}
