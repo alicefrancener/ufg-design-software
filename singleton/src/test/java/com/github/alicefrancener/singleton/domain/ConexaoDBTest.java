eb51e92fff594a4b83db213b8a50769f04ec964d
kyriosdata <fabio@inf.ufg.br>
2019-09-12 12:54:08 -0300

    menor privilegio é melhor

--- singleton/src/test/java/com/github/alicefrancener/singleton/domain/ConexaoDBTest.java
+++ singleton/src/test/java/com/github/alicefrancener/singleton/domain/ConexaoDBTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ConexaoDBTest {
+class ConexaoDBTest {
 
     @Test
     void duasConexoesMesmaInstancia() {
