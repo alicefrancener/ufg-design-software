107197ba86412d3a3eab222ace58e6c9ae201bc4
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2019-09-24 10:15:59 -0300

    Teste mais "forte"

--- classe/src/test/java/com/github/alicefrancener/classe/domain/ClasseTest.java
+++ classe/src/test/java/com/github/alicefrancener/classe/domain/ClasseTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ClasseTest {
+    // FIXME veja estratégia adotada pelo Josenilton (diria que é sólida e não possui dependência de contexto, como aquela abaixo). 
+    // TODO veja https://github.com/newtonjose/ds-2019-02/blob/master/auto-closeable/src/test/java/com/github/newtonjose/autocloseable/FileInputStreamAutoCloseableTest.java
     @Test
     void verificaCloseChamado() {
         assertThrows(RuntimeException.class, () -> {
